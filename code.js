1725155434ddb49d76d3379623780920c5b34df6
CathK21 <Catherine.composer@gmail.com>
2016-04-06 20:41:52 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
 var johncena = function() {
   return "AND HIS NAME IS JOHN CENA";
 };
+johncena();
+var multiplytwofactors = function(factor,factor2) {
+  var answer = ;
+  factor * factor2 = answer;
+  console.log("Your answer is " + answer);
+};
+multiplytwofactors(5,7);
